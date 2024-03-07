20cf7a922c0d90f3a5362c293a521bebf716b930
Frederick <softcheck@outlook.com>
2024-03-07 14:34:19 +0800

    update

--- DB_Gen/5_DB_Stats.py
+++ DB_Gen/5_DB_Stats.py
@@ -41,16 +41,20 @@ current_directory = os.path.dirname(current_file_name)
 # Change the current working directory
 os.chdir(current_directory)
 
-
-# 创建一个宽高比为2:1的figure
-fig = plt.figure(figsize=(10, 5))     
+# Create a figure with aspect ratio 2:1
+fig = plt.figure(figsize=(10, 5))
 ax = fig.add_subplot(1, 1, 1)
-# 设置axes的宽高比为3:2
+
+# Set the aspect ratio of axes to 3:2
 ax.set_aspect(3/2)
 
 start_time = time.time()
 
+
 def download_progress(count, block_size, total_size):
+    """
+    This function is used to track the progress of a download.
+    """
     global start_time
     duration = time.time() - start_time
     progress_size = int(count * block_size)
@@ -59,121 +63,158 @@ def download_progress(count, block_size, total_size):
     print(f"Downloaded {progress_size} of {total_size} bytes ({percent}% done), speed {speed} KB/s")
     if progress_size >= total_size:  # reset start_time for next download
         start_time = time.time()
-# Download the database file
-filename = "GeoRoc.db"
-zip_file = "GeoRoc.zip"
-url = quote("https://github.com/GeoPyTool/GeoRocData/raw/main/GeoRoc.zip")
 
-if not os.path.exists(filename):
-    if os.path.exists(zip_file):
-        with zipfile.ZipFile(zip_file, 'r') as zip_ref:
-            # zip_ref.extractall('.')
-            total_files = len(zip_ref.infolist())
-            extracted_files = 0
-            for file in zip_ref.infolist():
-                zip_ref.extract(file)
-                extracted_files += 1
-                print(f'Unzip Progress: {extracted_files / total_files * 100:.2f}%')
-            # os.remove(zip_file)
-    else:
-        urllib.request.urlretrieve(url, zip_file, reporthook=download_progress)
-        with zipfile.ZipFile(zip_file, 'r') as zip_ref:
-            # zip_ref.extractall('.')
-            total_files = len(zip_ref.infolist())
-            extracted_files = 0
-            for file in zip_ref.infolist():
-                zip_ref.extract(file)
-                extracted_files += 1
-                print(f'Unzip Progress: {extracted_files / total_files * 100:.2f}%')
 
-# Connect to the database
-conn = sqlite3.connect(filename)
+def download_and_extract(filename, zip_file, url, extract_dir):
+    """
+    This function downloads a zip file from a given URL, extracts it, and opens the directory in the file explorer.
 
-# Read the data from the Current_Data table
-df = pd.read_sql_query("SELECT * FROM Current_Data", conn)
-# 输出'Type'的取值个数
-# print(df['Type'].value_counts())
+    Args:
+    filename (str): The name of the file to check if it exists.
+    zip_file (str): The name of the zip file to download.
+    url (str): The URL to download the zip file from.
+    extract_dir (str): The directory to extract the zip file to.
+    """
+    if not os.path.exists(filename):
+        if os.path.exists(zip_file):
+            with zipfile.ZipFile(zip_file, 'r') as zip_ref:
+                total_files = len(zip_ref.infolist())
+                extracted_files = 0
+                for file in zip_ref.infolist():
+                    zip_ref.extract(file, path=extract_dir)
+                    extracted_files += 1
+                    print(f'Unzip Progress: {extracted_files / total_files * 100:.2f}%')
+        else:
+            urllib.request.urlretrieve(url, zip_file, reporthook=download_progress)
+            with zipfile.ZipFile(zip_file, 'r') as zip_ref:
+                total_files = len(zip_ref.infolist())
+                extracted_files = 0
+                for file in zip_ref.infolist():
+                    zip_ref.extract(file, path=extract_dir)
+                    extracted_files += 1
+                    print(f'Unzip Progress: {extracted_files / total_files * 100:.2f}%')
+
+        print(f'{zip_file} has been downloaded and extracted to \n {os.path.abspath(extract_dir)}')
+
+        # Open the directory in the file explorer
+        if platform.system() == "Windows":
+            os.startfile(os.path.abspath(extract_dir))
+        elif platform.system() == "Darwin":
+            os.system(f'open "{os.path.abspath(extract_dir)}"')
+        else:
+            os.system(f'xdg-open "{os.path.abspath(extract_dir)}"')
 
 
 def visualize_data(df, name):
-    data  = (df[(df['ROCK_TYPE'] == name) ]['Type'].value_counts())
-    data.to_csv(name+'_type_list.csv')
+    """
+    This function is used to visualize the data.
+    """
+    data = (df[(df["ROCK TYPE"] == name)]['Type'].value_counts())
 
-    # 设置黄金分割比例
+    # Set the golden ratio
     golden_ratio = 1.618
 
-    # 设置图形的宽度和高度
+    # Set the width and height of the figure
     fig_width = 10
     fig_height = fig_width / golden_ratio
 
     fig, ax = plt.subplots(figsize=(fig_width, fig_height))
 
-    # 对数据进行排序
+    # Sort the data
     data_sorted = data.sort_values(ascending=False)
 
-
-    # 计算样品数量的中位数
+    # Calculate the median of the sample quantity
     median = data_sorted.median()
 
-    # 使用坐标轴的方法在图上绘制一条水平线表示中位数
+    # Draw a horizontal line to represent the median using the axes method
     ax.axhline(y=median, color='k', linestyle='--')
 
-    # 使用坐标轴的方法创建一个垂直的柱状图
+    # Create a vertical bar chart using the axes method
     colors = ['black' if x >= median else 'gray' for x in data_sorted.values]
-    bars = ax.bar(data_sorted.index, data_sorted.values, color=colors,alpha = 0.3)
-    # 在每个柱条的内部最高处添加对应的数值
+    bars = ax.bar(data_sorted.index, data_sorted.values, color=colors, alpha=0.3)
+
+    # Add the corresponding value at the highest point inside each bar
     for bar in bars:
         height = bar.get_height()
         if height >= median:
             ax.text(bar.get_x() + bar.get_width() / 2, height,
                     '{:d}'.format(int(height)), ha='center', va='bottom', rotation=90)
 
-    # 找到第一个低于中位数的值的索引
+    # Find the index of the first value below the median
     first_below_median_index = next(i for i, x in enumerate(data_sorted.values) if x < median)
-    # 获取对应的x值
+
+    # Get the corresponding x value
     first_below_median_x = data_sorted.index[first_below_median_index]
-    # 在中位线旁边添加中位数的值
+
+    # Add the value of the median next to the median line
     ax.text(first_below_median_x, median, 'Median: {:.2f}'.format(median), color='k', va='bottom')
 
-    # 设置x轴标签的颜色
+    # Set the color of the x-axis labels
     for ticklabel, tickcolor in zip(ax.get_xticklabels(), colors):
         ticklabel.set_color(tickcolor)
-    
-    # 找到所有大于30的值的索引
+
+    # Find the indices of all values greater than 30
     indices_gt_30 = [i for i, x in enumerate(data_sorted.values) if x > 30]
 
-    # 在图的右上部分显示这些索引和对应的值，分成三列
+    # Display these indices and their corresponding values in the upper right part of the graph, divided into three columns
     for idx, i in enumerate(indices_gt_30):
         color = 'black' if data_sorted.values[i] >= median else 'gray'
-        if idx % 3 == 0:  # 对于模数为0的索引，放在左列
+        if idx % 3 == 0:  # For indices with a modulus of 0, put them in the left column
             ax.text(0.33, 1 - 0.05 * (idx // 3) - 0.1, ' {}: {}'.format(data_sorted.index[i], data_sorted.values[i]),
                     color=color, horizontalalignment='right', verticalalignment='top', transform=ax.transAxes)
-        elif idx % 3 == 1:  # 对于模数为1的索引，放在中列
+        elif idx % 3 == 1:  # For indices with a modulus of 1, put them in the middle column
             ax.text(0.66, 1 - 0.05 * (idx // 3) - 0.1, ' {}: {}'.format(data_sorted.index[i], data_sorted.values[i]),
                     color=color, horizontalalignment='right', verticalalignment='top', transform=ax.transAxes)
-        else:  # 对于模数为2的索引，放在右列
+        else:  # For indices with a modulus of 2, put them in the right column
             ax.text(1, 1 - 0.05 * (idx // 3) - 0.1, ' {}: {}'.format(data_sorted.index[i], data_sorted.values[i]),
                     color=color, horizontalalignment='right', verticalalignment='top', transform=ax.transAxes)
 
-    # 使用坐标轴的方法添加标题和标签
-    ax.set_title('Data Count of '+name+' Type')
-    # ax.set_xlabel('Type')
+    # Add title and labels using the axes method
+    ax.set_title('Data Count of ' + name + ' Type')
     ax.set_ylabel('Number of Data Entries')
-    # 设置x轴的右侧限制
+
+    # Set the right limit of the x-axis
     ax.set_xlim(right=data_sorted.index[-1])
-    # 旋转x轴y轴的标签
-    plt.xticks(rotation=45, ha='right',fontsize=9)
+
+    # Rotate the labels of the x-axis and y-axis
+    plt.xticks(rotation=45, ha='right', fontsize=9)
     plt.yticks(rotation=90)
-    # 去掉图框的上方和右方的框线
+
+    # Remove the top and right frames of the graph
     ax.spines['top'].set_visible(False)
     ax.spines['right'].set_visible(False)
-    # 自动调整图形
-    fig.tight_layout()
-    # 保存图形为SVG和PNG文件
-    fig.savefig(name+'_data_stats.svg')
-    fig.savefig(name+'_data_stats.jpg',dpi=600)
 
+    # Adjust the figure automatically
+    fig.tight_layout()
+
+    # Save the figure as SVG and PNG files
+    output_dir = 'Db_Stats'
+    if not os.path.exists(output_dir):
+        os.makedirs(output_dir)
+
+    fig.savefig(output_dir + '/' + name + '_Stats.svg')
+    fig.savefig(output_dir + '/' + name + '_Stats.jpg', dpi=600)
+    data.to_csv(output_dir + '/' + name + '_List.csv')
+
+
+# Define the filename, zip file, URL, and extraction directory
+filename = "GeoRoc.db"
+zip_file = "GeoRoc.zip"
+url = "https://github.com/GeoPyTool/GeoRocData/raw/main/GeoRoc.zip"
+extract_dir = "./"
+
+# Download and extract the file
+download_and_extract(filename, zip_file, url, extract_dir)
+
+# Connect to the database
+conn = sqlite3.connect(filename)
+
+# Read the data from the Current_Data table
+df = pd.read_sql_query("SELECT * FROM Current_Data", conn)
+
+# Visualize the data
 visualize_data(df, name='VOL')
 visualize_data(df, name='PLU')
 
+# Close the connection to the database
 conn.close()
