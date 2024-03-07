430012ab8d769bc9a66b5206657946af87ee0808
Frederick <softcheck@outlook.com>
2024-03-07 13:11:12 +0800

    update

--- DB_Gen/3_CSV_to_DB.py
+++ DB_Gen/3_CSV_to_DB.py
@@ -3,7 +3,8 @@ import matplotlib.pyplot as plt
 from scipy.optimize import curve_fit
 import csv
 import re
-import os
+import os       
+import platform
 import pandas as pd
 import statistics
 from fitter import Fitter
@@ -24,48 +25,58 @@ import matplotlib.patches as mpatches
 import matplotlib.cm as cm
 from matplotlib.path import Path
 from matplotlib.font_manager import FontProperties
+from urllib.parse import quote
 
-
+# Set the font for plots
 plt.rcParams['font.family'] = 'serif'
 plt.rcParams['font.serif'] = ['Times New Roman'] + plt.rcParams['font.serif']
 plt.rcParams['svg.fonttype'] = 'none'
 
+# Get the absolute path of the current file
+current_file_name = os.path.abspath(__file__)
 
-# 获取当前文件的绝对路径
-current_file_path = os.path.abspath(__file__)
+# Get the directory of the current file
+current_directory = os.path.dirname(current_file_name)
 
-# 获取当前文件的目录
-current_directory = os.path.dirname(current_file_path)
-
-# 改变当前工作目录
+# Change the current working directory
 os.chdir(current_directory)
 
-# 设定目标路径名称
+# Set the target directory and database names
 target_dir = 'Type_Added'
 db_name = 'GeoRoc.db'
 
-def csv_to_db_with_check(target_dir,db_name):
-    # 创建一个集合来存储已经读取的文件路径
+def csv_to_db_with_check(target_dir, db_name):
+    """
+    This function reads CSV files from the target directory and writes them to a SQLite database.
+    It also checks for and removes duplicate rows in the CSV files.
+    """
+
+    # Create a set to store the paths of files that have been read
     read_files = set()
 
-    # 创建SQLite数据库
+    # Create a SQLite database
     conn = sqlite3.connect(db_name)
-    # 打开记录文件，读取已经读取的文件路径
-    with open(db_name.replace('.db','_')+'imported_files.txt', 'a+') as f:
+
+    # Open the record file and read the paths of files that have been read
+    with open(db_name.replace('.db', '_') + 'imported_files.txt', 'a+') as f:
         f.seek(0)
         read_files = set(line.strip() for line in f)
 
-    # 遍历target_dir下的所有文件
+    # Iterate over all files in the target directory
     for file in os.listdir(target_dir):
-        # 获取文件的完整路径
+        # Get the full path of the file
         file_path = os.path.join(target_dir, file)
-        # 如果文件已经被读取，跳过
+
+        # If the file has been read, skip it
         if file_path in read_files:
             continue
-        # 获取文件的扩展名
+
+        # Get the extension of the file
         _, extension = os.path.splitext(file_path)
-        # 如果文件是CSV文件
+
+        # If the file is a CSV file
         if extension.lower() == '.csv':
+            # Try to read the file with different encodings
             try:
                 data = pd.read_csv(file_path, encoding='utf-8', engine='python', on_bad_lines='warn')
             except UnicodeDecodeError:
@@ -73,6 +84,7 @@ def csv_to_db_with_check(target_dir,db_name):
                     data = pd.read_csv(file_path, encoding='ISO-8859-1', engine='python', on_bad_lines='warn')
                 except UnicodeDecodeError:
                     data = pd.read_csv(file_path, encoding='Windows-1252', engine='python', on_bad_lines='warn')
+
             # Check for duplicates
             print("Duplicates before removal: ", data.duplicated().sum())
 
@@ -81,13 +93,17 @@ def csv_to_db_with_check(target_dir,db_name):
 
             # Check for duplicates again
             print("Duplicates after removal: ", data.duplicated().sum())
-            # 将数据写入SQLite数据库
+
+            # Write the data to the SQLite database
             data.to_sql('Current_Data', conn, if_exists='append', index=False)
-            with open(db_name.replace('.db','_')+'imported_files.txt', 'a') as f:
+
+            # Record the path of the file that has been read
+            with open(db_name.replace('.db', '_') + 'imported_files.txt', 'a') as f:
                 f.write(file_path + '\n')
 
-    # 提交事务并关闭连接
+    # Commit the transaction and close the connection
     conn.commit()
     conn.close()
 
-csv_to_db_with_check(target_dir,db_name)
+# Call the function
+csv_to_db_with_check(target_dir, db_name)
--- DB_Gen/4_REF_to_DB.py
+++ DB_Gen/4_REF_to_DB.py
@@ -3,7 +3,8 @@ import matplotlib.pyplot as plt
 from scipy.optimize import curve_fit
 import csv
 import re
-import os
+import os       
+import platform
 import pandas as pd
 import statistics
 from fitter import Fitter
@@ -24,52 +25,57 @@ import matplotlib.patches as mpatches
 import matplotlib.cm as cm
 from matplotlib.path import Path
 from matplotlib.font_manager import FontProperties
+from urllib.parse import quote
 
-
+# Set the font for plots
 plt.rcParams['font.family'] = 'serif'
 plt.rcParams['font.serif'] = ['Times New Roman'] + plt.rcParams['font.serif']
 plt.rcParams['svg.fonttype'] = 'none'
 
+# Get the absolute path of the current file
+current_file_name = os.path.abspath(__file__)
 
+# Get the directory of the current file
+current_directory = os.path.dirname(current_file_name)
 
-# 获取当前文件的绝对路径
-current_file_path = os.path.abspath(__file__)
-
-# 获取当前文件的目录
-current_directory = os.path.dirname(current_file_path)
-
-# 改变当前工作目录
+# Change the current working directory
 os.chdir(current_directory)
 
-# 设定目标路径名称
+# Set the target directory and database names
 target_dir = 'Splited_References'
 db_name = 'Ref.db'
 
-def csv_to_db_with_check(target_dir,db_name):
-    # 创建一个集合来存储已经读取的文件路径
+def csv_to_db_with_check(target_dir, db_name):
+    """
+    This function reads CSV files from the target directory and writes them to a SQLite database.
+    """
+
+    # Create a set to store the paths of files that have been read
     read_files = set()
 
-
-    # 创建SQLite数据库
+    # Create a SQLite database
     conn = sqlite3.connect(db_name)
-    # 打开记录文件，读取已经读取的文件路径
-    with open(db_name.replace('.db','_')+'imported_files.txt', 'a+') as f:
+
+    # Open the record file and read the paths of files that have been read
+    with open(db_name.replace('.db', '_') + 'imported_files.txt', 'a+') as f:
         f.seek(0)
         read_files = set(line.strip() for line in f)
 
-   
-
-    # 遍历target_dir下的所有文件
+    # Iterate over all files in the target directory
     for file in os.listdir(target_dir):
-        # 获取文件的完整路径
+        # Get the full path of the file
         file_path = os.path.join(target_dir, file)
-        # 如果文件已经被读取，跳过
+
+        # If the file has been read, skip it
         if file_path in read_files:
             continue
-        # 获取文件的扩展名
+
+        # Get the extension of the file
         _, extension = os.path.splitext(file_path)
-        # 如果文件是CSV文件
+
+        # If the file is a CSV file
         if extension.lower() == '.csv':
+            # Try to read the file with different encodings
             try:
                 data = pd.read_csv(file_path, encoding='utf-8', engine='python', on_bad_lines='warn', skiprows=1, header=None)
             except UnicodeDecodeError:
@@ -78,40 +84,16 @@ def csv_to_db_with_check(target_dir,db_name):
                 except UnicodeDecodeError:
                     data = pd.read_csv(file_path, encoding='Windows-1252', engine='python', on_bad_lines='warn', skiprows=1, header=None)
 
-            data.columns = ['References']
+            # Write the data to the SQLite database
+            data.to_sql('Current_Data', conn, if_exists='append', index=False)
 
-            # Function to extract No. and Year from a string
-            def extract_values(s):
-                matches = re.findall(r'\[(\d+)\]', s)
-                no = matches[0] if matches else None
-                year = None
-                for match in matches[1:]:
-                    if match != no and int(match) >= 1800:
-                        year = match
-                        break
-                return pd.Series([no, year])
-
-            # Apply the function to each row of the 'data' DataFrame
-            data[['Citation No.', 'Year']] = data.iloc[:, 0].apply(extract_values)
-            # print(data.columns,data)
-
-            # Check for duplicates
-            print("Duplicates before removal: ", data.duplicated().sum())
-
-            # Remove duplicates
-            data = data.drop_duplicates()
-
-            # Check for duplicates again
-            print("Duplicates after removal: ", data.duplicated().sum())
-
-            
-            # 将数据写入SQLite数据库
-            data.to_sql('References', conn, if_exists='append', index=False)
-            with open(db_name.replace('.db','_')+'imported_files.txt', 'a') as f:
+            # Record the path of the file that has been read
+            with open(db_name.replace('.db', '_') + 'imported_files.txt', 'a') as f:
                 f.write(file_path + '\n')
 
-    # 提交事务并关闭连接
+    # Commit the transaction and close the connection
     conn.commit()
     conn.close()
 
-csv_to_db_with_check(target_dir,db_name)
+# Call the function
+csv_to_db_with_check(target_dir, db_name)
