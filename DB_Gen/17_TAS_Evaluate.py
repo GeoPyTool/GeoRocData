f99afffdb54e4fff2718209109a7ccf61f588caa
Frederick <softcheck@outlook.com>
2024-03-18 14:09:16 +0800

    update

--- DB_Gen/17_TAS_Evaluate.py
+++ DB_Gen/17_TAS_Evaluate.py
@@ -84,10 +84,28 @@ df = df.reindex(columns=new_order)
 
 df.to_csv('sampled_TAS_data_evaluated.csv', index=False)
 
+# 创建一个空的DataFrame来存储结果
+results_dict ={}
+
 # 对每个测试列进行计算
 for test in ['Max_Ratio Test', 'Soft-Max Test', 'TAS Test']:
     true_ratio = df[test].mean()
-    print(f"{test} True ratio: {true_ratio}")
+    results_dict[test]=(true_ratio)
+    
+print(results_dict.keys())
+print(results_dict)
+
+# 创建一个新的DataFrame，其中列名是results_dict的键，值是results_dict的值
+results_df = pd.DataFrame([results_dict.values()], columns=results_dict.keys())
+
+print(results_df)
+
+# 添加一个新的列"Label"，并设置所有的值为"Overall"
+results_df['Label'] = 'Overll'
+# 将"Label"列设置为results_df的索引
+results_df = results_df.set_index('Label')
+
+print(results_df,'\n\n\n')
 
 # 创建一个新的DataFrame来保存结果
 true_ratio_df = pd.DataFrame()
@@ -96,10 +114,75 @@ true_ratio_df = pd.DataFrame()
 for test in ['Max_Ratio Test', 'Soft-Max Test', 'TAS Test']:
     true_ratio_df[test] = df.groupby('Label')[test].mean().round(3)
 
+
+
 # 删除所有值都为0的行
 true_ratio_df = true_ratio_df.loc[~(true_ratio_df==0).all(axis=1)]
 
-# 查看结果
-print(true_ratio_df)
+# 按照'TAS Test'列的值对true_ratio_df进行排序
+# true_ratio_df = true_ratio_df.sort_values('TAS Test', ascending=False)
+# 按照'TAS Test', 'Soft-Max Test', 'Max_Ratio Test'列的值对true_ratio_df进行排序
+# true_ratio_df = true_ratio_df.sort_values(['TAS Test', 'Soft-Max Test', 'Max_Ratio Test'], ascending=False)
 
 true_ratio_df.to_csv('sampled_TAS_data_evaluated_result.csv', index= True)
+
+
+# 对'Max_Ratio Test', 'Soft-Max Test', 'TAS Test'列的值求和，并按照求和结果对true_ratio_df进行排序
+true_ratio_df = true_ratio_df.assign(sum_value=true_ratio_df[['Max_Ratio Test', 'Soft-Max Test', 'TAS Test']].sum(axis=1)).sort_values('sum_value')
+
+
+# 将results_df添加到true_ratio_df的底部
+true_ratio_df = pd.concat([results_df, true_ratio_df])
+
+print(true_ratio_df)
+
+# 创建一个新的figure和axes
+fig, ax = plt.subplots()
+
+# 定义一个列表来保存底部的高度
+bottom = np.zeros(len(true_ratio_df))
+
+import matplotlib.cm as cm
+
+# 定义一个颜色图
+# cmap = cm.get_cmap('viridis')
+# 定义一个颜色列表
+cmap = [[154/255, 153/255, 183/255], [228/255, 128/255, 128/255], [128/255, 128/255, 228/255]]
+
+# 对每个测试列进行绘图，并保存每个测试列的柱子
+bars_dict = {}
+for i, test in enumerate(['TAS Test', 'Max_Ratio Test', 'Soft-Max Test']):
+    color = cmap[i]  # 获取颜色列表中的颜色
+    bars = ax.bar(true_ratio_df.index, true_ratio_df[test], label=test, bottom=bottom, color=color)
+    bars_dict[test] = bars
+    for bar in bars:
+        height = bar.get_height()
+        # 如果高度不为0，则显示对应的值
+        if height != 0:
+            y = bar.get_y() + bar.get_height() / 2
+            ax.text(bar.get_x() + bar.get_width() / 2, y, 
+                f'{height:.2f}', ha='center', va='center', rotation=90, color='white', fontsize=9)
+            
+    bottom += true_ratio_df[test]
+
+# 添加图例，图例的顺序是['Max_Ratio Test', 'Soft-Max Test', 'TAS Test']
+handles = [bars_dict[test] for test in ['Soft-Max Test','Max_Ratio Test', 'TAS Test']]
+ax.legend(handles=handles)
+
+# 添加标题和标签
+ax.set_title('True Ratio for Each Test')
+# ax.set_xlabel('Label')
+ax.set_ylabel('True Ratio')
+
+# 设置x轴标签的倾斜角度
+plt.xticks(rotation=45, ha='right')
+
+# 调整子图参数
+fig.tight_layout()
+
+plt.savefig('sampled_TAS_data_evaluated_result.jpg', dpi=600)
+plt.savefig('sampled_TAS_data_evaluated_result.pdf')
+plt.savefig('sampled_TAS_data_evaluated_result.svg')
+
+# 显示图形
+plt.show()
